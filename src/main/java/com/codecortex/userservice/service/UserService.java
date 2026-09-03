db517757337ef217102b1a1bb64bd473b461bde3
Jaiswalroshan <90549641+Jaiswalroshan@users.noreply.github.com>
2026-09-03 16:13:19 +0530

    Refactor createOrUpdateUser method to streamline user update and creation logic

--- src/main/java/com/codecortex/userservice/service/UserService.java
+++ src/main/java/com/codecortex/userservice/service/UserService.java
@@ -18,19 +18,38 @@ public class UserService {
     @Autowired
     private final UserRepository userRepository;
 
-    public UserInfoDto createOrUpdateUser(UserInfoDto userInfoDto){
-        UnaryOperator<UserInfo> updatingUser = user -> {
-            return userRepository.save(userInfoDto.transformToUserInfo());
-        };
+    public UserInfoDto createOrUpdateUser(UserInfoDto userInfoDto) {
+//        UnaryOperator<UserInfo> updatingUser = user -> {
+//            return userRepository.save(userInfoDto.transformToUserInfo());
+//        };
+//
+//
+//        Supplier<UserInfo> createUser = () -> {
+//            return userRepository.save(userInfoDto.transformToUserInfo());
+//        };
+//
+//        UserInfo userInfo = userRepository.findByUserId(userInfoDto.getUserId())
+//                .map(updatingUser)
+//                .orElseGet(createUser);
+
+        UserInfo userInfo = userRepository
+                .findByUserId(userInfoDto.getUserId())
+                .map(existingUser -> {
+                    existingUser.setFirstName(userInfoDto.getFirstName());
+                    existingUser.setLastName(userInfoDto.getLastName());
+                    existingUser.setPhoneNumber(userInfoDto.getPhoneNumber());
+                    existingUser.setEmail(userInfoDto.getEmail());
+                    existingUser.setProfilePic(userInfoDto.getProfilePic());
+                    return userRepository.save(existingUser);
+                })
+                .orElseGet(() -> {
+                            UserInfo newUser = userInfoDto.transformToUserInfo();
+                            return userRepository.save(newUser);
+                        }
+
+                );
 
 
-        Supplier<UserInfo> createUser = () -> {
-            return userRepository.save(userInfoDto.transformToUserInfo());
-        };
-
-        UserInfo userInfo = userRepository.findByUserId(userInfoDto.getUserId())
-                .map(updatingUser)
-                .orElseGet(createUser);
         return new UserInfoDto(
                 userInfo.getUserId(),
                 userInfo.getFirstName(),
@@ -42,15 +61,12 @@ public class UserService {
     }
 
 
-
-
-
     public UserInfoDto getUser(UserInfoDto userInfoDto) throws Exception {
         Optional<UserInfo> userInfoDtoOpt = userRepository.findByUserId(userInfoDto.getUserId());
-        if(userInfoDtoOpt.isEmpty()){
+        if (userInfoDtoOpt.isEmpty()) {
             throw new Exception("User not found");
         }
-        UserInfo userInfo= userInfoDtoOpt.get();
+        UserInfo userInfo = userInfoDtoOpt.get();
 
         return new UserInfoDto(
                 userInfo.getUserId(),
